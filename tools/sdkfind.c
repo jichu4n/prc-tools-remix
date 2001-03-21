8855537b91f6a9f416f27d3a11f44df79d439287
jmarshall <jmarshall>
2001-03-21 02:02:19 +0000

    Undo the twisty turny -palmosX spec file stuff that I spent such a lot of time on eighteen months ago.  That method always wanted just another couple of features in the specs parser to work properly, and, while that was tempting :-), I was always conscious that the GCC maintainers' constant refrain when asked about such things was always "just tell the users to wrap a script around GCC".  (Gak!)
    So m68k-palmos-sdkfind is a wrapper to be used instead of GCC :-).
    It's not so horrible.  The real m68k-palmos-gcc now knows nothing
    about /usr/local/palmdev and makes no attempt to supply -I options
    to see in there.  Instead, when sdkfind is installed in the right
    way, it will rewrite any -palmosX options into mountains of -isystem
    and -L options, and then call down to the real m68k-palmos-gcc.  It
    also acts as a filter, so "m68k-palmos-gcc foo.c `m68k-palmos-sdkfind`"
    will do the right thing.
    
    With this patch it gets built and installed, but not plugged in.
    We'll probably want to rename the installed m68k-palmos-gcc as
    real-m68k-palmos-gcc, and install sdkfind as m68k-palmos-gcc.
    Or something.  It's all pretty tacky.  Perhaps there are other
    solutions that balance confusion and tackiness better...
    
    The handspring.palmos.diff patch is no longer necessary, since
    sdkfind will automatically look inside an include/Handspring
    directory if there is one.

---
+++ tools/sdkfind.c
@@ -0,0 +1,231 @@
+/* sdkfind.c: generate -I options etc for all subdirectories of an SDK.
+
+   Copyright (c) 2001 Palm, Inc. or its subsidiaries.
+   All rights reserved.
+
+   This is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#include "libiberty.h"
+
+#include "utils.h"
+
+
+const char *progname = "sdkfind";
+const char *progversion = "1.0";
+
+
+char **args = NULL, **arglim = NULL, **argmax = NULL;
+struct string_store *store;
+
+void
+add_space (int n) {
+  ptrdiff_t nlim = (args)? arglim - args : 0;
+  ptrdiff_t nmax = (args)? argmax - args : 0;
+  nmax += n;
+  args = xrealloc (args, nmax * sizeof (const char *));
+  arglim = args + nlim;
+  argmax = args + nmax;
+  }
+
+void
+add (char *s) {
+  if (arglim == argmax)
+    add_space (32);
+  *arglim++ = s;
+  }
+
+
+void
+add_isystem (const char *path) {
+  add ("-isystem");
+  add (insert_string (store, path));
+  }
+
+void
+add_L (const char *path) {
+  char buf[2 + FILENAME_MAX];
+  strcpy (buf, "-L");
+  strcat (buf, path);
+  add (insert_string (store, buf));
+  }
+
+
+int
+is_dir (const char *path) {
+  struct stat st;
+  return stat (path, &st) == 0 && S_ISDIR (st.st_mode);
+  }
+
+int
+add_tree (void (*addf) (const char *), const char *dirname) {
+  DIR *dir;
+
+  if (is_dir (dirname) && (dir = opendir (dirname)) != NULL) {
+    struct dirent *dirent;
+
+    addf (dirname);
+
+    while ((dirent = readdir (dir)) != NULL)
+      if (dirent->d_name[0] != '.') {
+	char fname[FILENAME_MAX];
+	sprintf (fname, "%s/%s", dirname, dirent->d_name);
+	add_tree (addf, fname);
+	}
+
+    closedir (dir);
+    return 1;
+    }
+  else
+    return 0;
+  }
+
+void
+add_trees (const char *sdk_version) {
+  char sdk[FILENAME_MAX];
+  char path[FILENAME_MAX];
+
+  int broken_sdk = 0;
+
+  sprintf (sdk, "sdk%s%s", (*sdk_version)? "-" : "", sdk_version);
+
+  sprintf (path, "%s/include", PALMDEV_PREFIX);
+  add_tree (add_isystem, path);
+
+  sprintf (path, "%s/%s/include", PALMDEV_PREFIX, sdk);
+  if (!add_tree (add_isystem, path))
+    broken_sdk = 1;
+
+  sprintf (path, "%s/lib/%s", PALMDEV_PREFIX, TARGET);
+  add_tree (add_L, path);
+
+  sprintf (path, "%s/%s/lib/%s", PALMDEV_PREFIX, sdk, TARGET);
+  if (!add_tree (add_L, path))
+    broken_sdk = 1;
+
+  sprintf (path, "%s/%s", PALMDEV_PREFIX, sdk);
+  if (!is_dir (path))
+    einfo (E_NOFILE|E_WARNING, "%s does not exist", path);
+  else if (broken_sdk)
+    einfo (E_NOFILE|E_WARNING, "%s does not contain an sdk", path);
+  }
+
+
+const char *
+find_subcmd (const char *cmd) {
+  static char path[FILENAME_MAX];
+  struct stat st;
+
+  sprintf (path, "%s/%s/bin/%s%s", EXEC_PREFIX, TARGET_ALIAS, cmd, EXEEXT);
+  if (stat (path, &st) == 0)
+    return path;
+
+  sprintf (path, "%s/bin/real-%s-%s%s", EXEC_PREFIX, TARGET_ALIAS, cmd, EXEEXT);
+  if (stat (path, &st) == 0)
+    return path;
+
+  return NULL;
+  }
+
+
+int
+prefix_strcmp (const char *s, const char *prefix) {
+  return strncmp (s, prefix, strlen (prefix));
+  }
+
+void
+dump (char **ap) {
+  while (*ap)
+    printf (" %s", *ap++);
+  putchar ('\n');
+  }
+
+
+int
+main (int argc, char **argv) {
+  int verbose_seen, palmos_seen, retcode, i;
+  const char *subcmd;
+
+  xmalloc_set_program_name (progname);
+  retcode = EXIT_SUCCESS;
+
+  subcmd = basename (argv[0]);
+  if (prefix_strcmp (subcmd, TARGET"-") == 0)
+    subcmd += strlen (TARGET"-");
+  else if (prefix_strcmp (subcmd, TARGET_ALIAS"-") == 0)
+    subcmd += strlen (TARGET_ALIAS"-");
+
+  add_space (argc);
+  store = new_string_store ();
+
+  verbose_seen = palmos_seen = 0;
+
+  for (i = 0; i < argc; i++)
+    if (prefix_strcmp (argv[i], "-palmos") == 0) {
+      add_trees (argv[i] + strlen ("-palmos"));
+      palmos_seen = 1;
+      }
+    else if (strcmp (argv[i], "-nopalmos") == 0)
+      palmos_seen = 1;
+    else {
+      if (strcmp (argv[i], "-v") == 0)
+	verbose_seen = 1;
+      add (argv[i]);
+      }
+
+  if (!palmos_seen)
+    add_trees ("");
+
+  add (NULL);
+
+  if (strcmp (subcmd, progname) == 0)
+    dump (args + 1);
+  else {
+    char *errmsg_fmt, *errmsg_arg;
+    int pid, status;
+
+    const char *full_subcmd = find_subcmd (subcmd);
+
+    if (verbose_seen) {
+      if (full_subcmd)
+	printf (" [%s]", full_subcmd);
+      dump (args);
+      }
+
+    if (!full_subcmd)
+      full_subcmd = subcmd;
+
+    pid = pexecute (full_subcmd, args, progname,
+		    NULL, &errmsg_fmt, &errmsg_arg, PEXECUTE_ONE);
+
+    if (pid >= 0) {
+      pwait (pid, &status, 0);
+      if (WIFSIGNALED (status))
+	retcode = EXIT_FAILURE;
+      else if (WIFEXITED (status))
+	retcode = WEXITSTATUS (status);
+      }
+    else
+      einfo (E_NOFILE|E_PERROR, errmsg_fmt, errmsg_arg);
+    }
+
+  free_string_store (store);
+  free (args);
+
+  if (nerrors > 0)
+    retcode = EXIT_FAILURE;
+
+  return retcode;
+  }
