5307bdcda5651eb81a97eb83d1a11e105ebce214
jmarshall <jmarshall>
2001-07-24 07:17:39 +0000

    Add GenBootstrap, which is a Palm OS application, compiled against a Palm OS SDK, which when run produces bootstrap*.h header files.  These can be used instead of an SDK by the rest of prc-tools.

---
+++ bootstrap/genbootstrap.h
@@ -0,0 +1,25 @@
+/* genbootstrap.h: generate bootstrap headers for compiling startup code.
+
+   Copyright 2001 by John Marshall.
+   <jmarshall@acm.org>
+
+   This is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.  */
+
+#ifndef GENBOOTSTRAP_H
+#define GENBOOTSTRAP_H
+
+void console (const char *);
+void filestream (const char *);
+
+void printf (const char *format, ...);
+void fprintf (void (*output) (const char *), const char *format, ...);
+
+void copy_if_different (const char *fname);
+
+void bootstrap (const char *);
+void bootstrap_ui (const char *);
+
+#endif
