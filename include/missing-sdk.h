8070247d2e43c1a4fac8c64036ac46282806d2be
jmarshall <jmarshall>
2002-04-25 17:08:14 +0000

    Add dummy Pilot.h and PalmOS.h files to an include directory searched after any directories in {PalmDev}.  Thus, if the SDK installation is screwed up or they used "#include <Pilot.h>" with a 3.5+ SDK or most importantly if they haven't run palmdev-prep yet, then our dummy file will get #included in lieu of a real one, and it contains #errors with more useful error messages than just "file not found".
    (This change removed all the non-trivial, non-copied from other
    makefiles, content, so the old copyright notice has been removed too.)

---
+++ include/missing-sdk.h
@@ -0,0 +1,17 @@
+/* This is not the real Pilot.h or PalmOS.h!
+
+   The genuine files are part of a Palm OS SDK, which is not part of prc-tools.
+   But it is a common error to install SDKs incorrectly, or not to install any
+   at all, or to use the old-style Pilot.h with a new-style SDK or vice versa,
+   and the following is not always a very useful diagnostic:
+
+	Pilot.h: No such file or directory
+
+   This placeholder file lets us provide a better explanation.  */
+
+<#error No genuine Pilot.h found
+>#error No genuine PalmOS.h found
+<#warning use PalmOS.h instead if you are using the 3.5 SDK or later; \
+>#warning use Pilot.h if you really mean to use the 3.1 SDK or earlier; \
+you may need to run palmdev-prep or use a suitable -palmosX option -- \
+see "http://prc-tools.sourceforge.net/cgi-bin/info/palmdev-prep" for details
