84396e1788f7daf1f328a177882a10bf1a8bfea5
jmarshall <jmarshall>
2002-06-06 15:35:40 +0000

    Remove email address from copyright notice.

--- tools/dirutils.c
+++ tools/dirutils.c
@@ -1,7 +1,6 @@
 /* dirutils.c: various filesystem-related utilities.
 
-   Copyright 2002 by John Marshall.
-   <jmarshall@acm.org>
+   Copyright 2002 John Marshall.
 
    This is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
