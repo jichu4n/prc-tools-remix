4a52cf7520086dd6d331d2cd6028413b6a2ee1d6
jmarshall <jmarshall>
2002-10-08 18:20:33 +0000

    Collect the new memcpy() and memset() implementations into a single source file, memstring.c (to which we'll be adding the other mem* functions), and use the usual -DLmemcpy trick in the makefile.
    This source code arrangement is less efficient because you have to
    pass all that text through the preprocessor several times, but the
    impact is trivial and IMHO outweighed by the advantages of having
    all the related source code together and of not drowning in masses
    of tiny source files.
    
    Removed the quotations from the Standard describing these functions,
    because we're not going to do that for every function, and because
    all interested parties either know well enough what these functions
    do or probably have a copy of the Standard anyway.

---
+++ libc/memstring.c
@@ -0,0 +1,83 @@
+/* memstring.c: ISO/IEC 9899:1999  7.21  String handling -- the mem* functions.
+
+   Placed in the public domain by John Marshall.  */
+
+#include "string.h"
+
+#ifdef __m68k__
+#define alignment(p)  ((unsigned long) (p) & 1)
+#define while_maybe   if
+#else
+#define alignment(p)  ((unsigned long) (p) & 3)
+#define while_maybe   while
+#endif
+
+#ifdef Lmemcpy
+
+void *
+memcpy (void *vdst, const void *vsrc, size_t n) {
+  char *dst = vdst;
+  const char *src = vsrc;
+
+  if (n >= 11 && alignment (dst) == alignment (src)) {
+    long *ldst;
+    const long *lsrc;
+    size_t nl;
+
+    while_maybe (alignment (src) != 0)  *dst++ = *src++, n--;
+
+    ldst = (long *) dst;
+    lsrc = (const long *) src;
+    nl = n / 4;
+
+    while (nl--)  *ldst++ = *lsrc++;
+
+    dst = (char *) ldst;
+    src = (const char *) lsrc;
+    n = n % 4;
+    }
+
+  while (n--)  *dst++ = *src++;
+
+  return vdst;
+  }
+
+#endif
+#ifdef Lmemset
+
+void *
+memset (void *vp, int ic, size_t n) {
+  unsigned char *p = vp;
+  unsigned char c = ic;
+
+  if (n >= 11) {
+    unsigned short cs;
+    unsigned long cl, *pl;
+    size_t nl;
+
+#ifdef __m68k__
+    if (((unsigned long) p) & 1)  *p++ = c, n--;
+#else
+    while (((unsigned long) p) & 3)  *p++ = c, n--;
+#endif
+
+    cs = c;
+    cs |= c << 8;
+    cl = cs;
+    cl |= cl << 16;
+
+    pl = (unsigned long *) p;
+    nl = n / 4;
+
+    while (nl--)  *pl++ = cl;
+
+    p = (unsigned char *) pl;
+    n = n % 4;
+    }
+
+  while (n--)  *p++ = c;
+
+  return vp;
+  }
+
+#endif
