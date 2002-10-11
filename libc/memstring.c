43dbbcf2718abb29d949542b14d16c90cad1a2f9
jmarshall <jmarshall>
2002-10-11 13:55:27 +0000

    Add a (pathetically simple) memchr() implementation.

--- libc/memstring.c
+++ libc/memstring.c
@@ -43,6 +43,23 @@ memcpy (void *vdst, const void *vsrc, size_t n) {
   }
 
 #endif
+#ifdef Lmemchr
+
+void *
+memchr (const void *sv, int ic, size_t n) {
+  const unsigned char *s = sv;
+  unsigned char c = ic;
+
+  while (n > 0) {
+    if (*s++ == c)
+      return (void *) --s;
+    n--;
+    }
+
+  return NULL;
+  }
+
+#endif
 #ifdef Lmemset
 
 void *
