029329fb2f569787a2bf8902af087dee4b81327d
jmarshall <jmarshall>
2002-10-18 13:01:11 +0000

    Replace the old Linux-derived memmove() and memcmp() routines with new ones implemented from scratch.  The new ones are better in several ways:
    * they copy/compare a (long) word at a time where possible;
    * their licensing (or really lack thereof) is crystal clear;
    * they actually work -- the old memcmp() did its comparision with
      a signed char, which is not big enough to get the sign right for
      both '\0' - '\xff' and '\xff' - '\0'.  This is the same bug that
      was in Palm OS's MemCmp() until I fixed it there too... *sigh*.

--- libc/memstring.c
+++ libc/memstring.c
@@ -43,6 +43,78 @@ memcpy (void *vdst, const void *vsrc, size_t n) {
   }
 
 #endif
+#ifdef Lmemmove
+
+void *
+memmove (void *vdest, const void *vsrc, size_t n) {
+  union { void *v; char *c; long *l; } dest = { vdest };
+  union { const char *c; const long *l; } src = { vsrc };
+
+  if (dest.c < src.c)
+    return memcpy (vdest, vsrc, n);
+  else {
+    dest.c += n;
+    src.c += n;
+
+    if (n >= 11 && alignment (dest.c) == alignment (src.c)) {
+      size_t nl;
+
+      while_maybe (alignment (src.c) != 0)  *--dest.c = *--src.c, n--;
+
+      nl = n / 4;
+      n = n % 4;
+
+      while (nl--)  *--dest.l = *--src.l;
+      }
+
+    while (n--)  *--dest.c = *--src.c;
+
+    return dest.v;
+    }
+  }
+
+#endif
+#ifdef Lmemcmp
+
+int
+memcmp (const void *vs1, const void *vs2, size_t n) {
+  union { const unsigned char *c; const long *l; } s1 = { vs1 }, s2 = { vs2 };
+
+  if (n >= 11 && alignment (s1.c) == alignment (s2.c)) {
+    size_t nl;
+
+    while_maybe (alignment (s1.c) != 0) {
+      int diff = *s1.c++ - *s2.c++;
+      if (diff != 0)
+	return diff;
+      n--;
+      }
+
+    nl = n / 4;
+    n = n % 4;
+
+    while (nl > 0) {
+      if (*s1.l++ != *s2.l++) {
+	s1.l--;
+	s2.l--;
+	n = 4;
+	break;
+	}
+      nl--;
+      }
+    }
+
+  while (n > 0) {
+    int diff = *s1.c++ - *s2.c++;
+    if (diff != 0)
+      return diff;
+    n--;
+    }
+
+  return 0;
+  }
+
+#endif
 #ifdef Lmemchr
 
 void *
