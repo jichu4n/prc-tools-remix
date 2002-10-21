3a65a124100557616a0052e9d5fb9917d9ca0b78
jmarshall <jmarshall>
2002-10-21 18:03:21 +0000

    Now that all the functions declared in this header have new public domain implementations, add the usual boilerplate.

--- include/string.h
+++ include/string.h
@@ -1,7 +1,19 @@
 /* string.h: ISO/IEC 9899:1999  7.21  String handling.
 
    Placed in the public domain by John Marshall.
-   */
+
+   This file is part of prc-tools, but is not licensed in the same way
+   as the majority of prc-tools.  The contents of this file, and also
+   the implementations of the functions declared in this file, are in
+   the public domain.
+
+   One particular consequence of this is that you may use these functions
+   in Palm OS executables without concern that doing so by itself causes
+   the resulting executable to be covered by any particular license, such as
+   the GNU General Public License that covers much of the rest of prc-tools.
+   However this does not invalidate any other reasons why the executable
+   might be covered by such a license (such as the use of other functions
+   from other header files that are so-covered).  */
 
 #ifndef _PRC_TOOLS_STRING_H
 #define _PRC_TOOLS_STRING_H
