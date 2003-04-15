0ad3a5c1e3f8791e4a8d337cebd36c035cda2ec3
jmarshall <jmarshall>
2003-04-15 23:13:42 +0000

    Add non-copyright header, and use the IMHO more legible "defined X" syntax instead of "defined(X)".

--- include/NewTypes.h
+++ include/NewTypes.h
@@ -1,3 +1,18 @@
+/* NewTypes.h: Detect the SDK in use, and ensure new-style types are defined.
+
+   Placed in the public domain by John Marshall.
+
+   This file is part of prc-tools, but is not licensed in the same way as the
+   majority of prc-tools.  The contents of this file are in the public domain.
+
+   One particular consequence of this is that you may use this header file
+   in Palm OS executables without concern that doing so by itself causes
+   the resulting executable to be covered by any particular license, such as
+   the GNU General Public License that covers much of the rest of prc-tools.
+   However this does not invalidate any other reasons why the executable
+   might be covered by such a license (such as the use of functions from
+   other header files that are so-covered).  */
+
 #ifndef NEWTYPES_H
 #define NEWTYPES_H
 
@@ -17,32 +32,32 @@
 
 /* 5.0 R2 did introduce a CPU_ARM macro, but more uniquely it also added
    some screen density constants.  */
-#if defined(SCREEN_DENSITY_STANDARD)
+#if defined SCREEN_DENSITY_STANDARD
 #define SDK_VERSION  52
 
 /* 5.0 should have introduced an ARM macro for use with CPU_TYPE, but the
    only change actually made was the introduction of a version macro.  */
-#elif defined(PALMOS_SDK_VERSION)
+#elif defined PALMOS_SDK_VERSION
 #define SDK_VERSION  50
 
 /* 4.0 introduced tracing.  */
-#elif defined(TRACE_OUTPUT_ON)
+#elif defined TRACE_OUTPUT_ON
 #define SDK_VERSION  40
 
 /* 3.5 uses <PalmTypes.h> instead of <Common.h>.  */
-#elif defined(__PALMTYPES_H__)
+#elif defined __PALMTYPES_H__
 #define SDK_VERSION  35
 
 /* 3.1 added Japanese support.  */
-#elif defined(LANGUAGE_JAPANESE)
+#elif defined LANGUAGE_JAPANESE
 #define SDK_VERSION  31
 
 /* 3.0 introduced the v2 memory manager.  */
-#elif defined(MEMORY_VERSION_2)
+#elif defined MEMORY_VERSION_2
 #define SDK_VERSION  30
 
 /* 2.0 added Italian support.  */
-#elif defined(LANGUAGE_ITALIAN)
+#elif defined LANGUAGE_ITALIAN
 #define SDK_VERSION  20
 
 #else
