51bc6d64add29add62bfa96cd3bfc2ae781aa67c
Chuan Ji <jichu4n@gmail.com>
2016-04-05 14:18:54 -0700

    Fix compile error in binutils.

--- binutils-2.14/gas/as.h
+++ binutils-2.14/gas/as.h
@@ -595,7 +595,19 @@ void print_dependencies PARAMS ((void));
 struct expressionS;
 struct fix;
 typedef struct symbol symbolS;
-struct relax_type;
+struct relax_type
+{
+  /* Forward reach. Signed number. > 0.  */
+  long rlx_forward;
+  /* Backward reach. Signed number. < 0.  */
+  long rlx_backward;
+
+  /* Bytes length of this address.  */
+  unsigned char rlx_length;
+
+  /* Next longer relax-state.  0 means there is no 'next' relax-state.  */
+  relax_substateT rlx_more;
+};
 typedef struct frag fragS;
 
 #ifdef BFD_ASSEMBLER
